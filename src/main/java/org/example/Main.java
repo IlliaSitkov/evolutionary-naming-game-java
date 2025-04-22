780e3b602503b389cf3e468e5acf50087fc513cb
Illia Sitkov <illiasitkov@gmail.com>
2025-04-22 14:03:24 +0300

    add run configs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -86,7 +86,7 @@ public class Main {
             // () -> AbruptTransitionRuns.original(40, 1, 0.0001),
             // () -> AbruptTransitionRuns.original(40, 1, 0.9),
             // () -> AbruptTransitionRuns.original(40, 1, 0)
-            () -> AbruptTransitionRuns.original(40, 1, 0.025)
+            // () -> AbruptTransitionRuns.original(40, 1, 0.05)
            
             // () -> AbruptTransitionRuns.originalMoloney(40, 1, 0.05)
             // () -> AbruptTransitionRuns.originalMoloney(40, 1, 0.025),
@@ -131,6 +131,16 @@ public class Main {
 
         //    () -> AbruptTransitionRuns.originalMoloney(40, 1, 0.05),
         //    () -> ContinuousPCommTransition.originalMoloney(40, 1)
+
+            // () -> ContinuousPCommTransition.original(30, 1, 0.5,0.05, 80000, 200000),
+            // () -> ContinuousPCommTransition.original(20, 1, 0.5,0.05, 80000, 200000),
+
+            () -> ContinuousPCommTransition.original(40, 5, 0.5,0.05, 80000, 200000),
+            () -> ContinuousPCommTransition.original(40, 1000, 0.5,0.05, 80000, 200000)
+
+            // todo:
+            // () -> ContinuousPCommTransition.original(40, 1, 0.5,0.005, 80000, 200000),
+            // () -> ContinuousPCommTransition.original(40, 1, 0.5,0.1, 80000, 200000)
 
         );
 
